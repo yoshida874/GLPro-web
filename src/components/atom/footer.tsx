f003fb433d576e1eac4abc2a30d21b588da290b4
yoshida874 <ud8mnzdd@gmail.com>
2021-11-01 09:49:45 +0900

    update: footerを一部修正

--- src/components/atom/footer.tsx
+++ src/components/atom/footer.tsx
@@ -13,12 +13,12 @@ const Footer: VFC = () => {
       padding={4}
     >
       <HStack spacing={8} mb={8}>
-        <Text color="gray.400">Privacy</Text>
-        <Text color="gray.400">Terms of Use</Text>
+        <Text>Privacy</Text>
+        <Text>Terms of Use</Text>
       </HStack>
       <Flex width="100%" justifyContent="center" wrap="wrap">
-        <Text width="50%" textAlign="center" color="gray.600" fontSize="sm">
-          Last Login 15.12.2020 - 14:00 | Data last updated on 16.12.20202 - 22:00
+        <Text width="50%" textAlign="center" fontSize="sm">
+          @.2021.GLPro
         </Text>
       </Flex>
     </Flex>
