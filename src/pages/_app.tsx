a5384e8e42d298e486ceae8854fca1a22d98db92
senzaki2525 <ryosuke2412@icloud.com>
2021-11-11 14:28:11 +0900

    update:ヘッダー追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider, Box } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import Header from '../components/atom/header/Header';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
       <Box background="#FAFAFA">
+        <Header />
         <Component {...pageProps} />
       </Box>
     </ChakraProvider>
