779dcd180952475c135371313bbf8c9771ded1d8
Ryosuke Senzaki <ase18410014@wiz.ac.jp>
2021-12-05 01:31:54 +0900

    update:DBへの投稿機能実装

--- src/pages/reviewpost.tsx
+++ src/pages/reviewpost.tsx
@@ -11,11 +11,11 @@ interface Result {
 }
 
 interface Reviews {
-  evaluation: number;
-  review_content: string;
-  category_id: number;
-  area_id: number;
   user_id: number;
+  area_id: number;
+  category_id: number;
+  review_content: string;
+  evaluation: number;
 }
 
 const reviews: Reviews[] = [];
@@ -30,41 +30,6 @@ const ReviewPost: NextPage = () => {
     6: { rate: 1, content: '' },
   });
 
-  // areaid取得
-  const router = useRouter();
-  // const areaId = router.query.id;
-  const areaId = 1;
-
-  const userid = 1;
-
-  const submitEvent = async () => {
-    Object.keys(result).map((key, index) => {
-      const onceData = result[key];
-
-      const data = {
-        evaluation: onceData.rate,
-        review_content: onceData.content,
-        category_id: index + 1,
-        area_id: areaId,
-        user_id: userid,
-      };
-      reviews.push(data);
-    });
-
-    const body = JSON.stringify({
-      Reviews: reviews,
-    });
-
-    // await axios
-    //   .post('http://localhost:8080/review/create', body)
-    //   .then((response) => {
-    //     console.log('成功: ' + response.data.text);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-  };
-
   const categories = [
     { id: 1, name: '物価' },
     { id: 2, name: '品揃え' },
@@ -73,6 +38,45 @@ const ReviewPost: NextPage = () => {
     { id: 5, name: '治安' },
     { id: 6, name: '交通' },
   ];
+
+  // areaid取得
+  const router = useRouter();
+  const areaId = Number(router.query.id);
+
+  // userid取得していなから定数で使用
+  const userid = 1;
+
+  const submitEvent = async () => {
+    // reviews配列に格納
+    Object.keys(result).map((key, index) => {
+      const onceData = result[key];
+
+      const data = {
+        user_id: userid,
+        area_id: areaId,
+        category_id: index + 1,
+        review_content: onceData.content,
+        evaluation: onceData.rate,
+      };
+      reviews.push(data);
+    });
+
+    //　reviewsをJson変換
+    const data = { Reviews: reviews };
+    const body = Buffer.from(JSON.stringify(data));
+
+    // Post
+    await axios
+      .post('http://localhost:8080/review/create', body, {
+        headers: { 'content-type': 'application/json' },
+      })
+      .then((response) => {
+        console.log('成功: ' + response.data.text);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <>
