20d1a895e985c3cbdc218343e444fca135507f45
Ryosuke Senzaki <ase18410014@wiz.ac.jp>
2022-01-14 13:59:58 +0900

    update:topページ更新

---
+++ src/components/top/TopCard.tsx
@@ -0,0 +1,36 @@
+import { VFC } from 'react';
+import { Box, Flex, Text, Image } from '@chakra-ui/react';
+
+interface Props {
+  title: string;
+  subtitle: string;
+  children: string;
+  img: string;
+}
+
+const TopCard: VFC<Props> = ({ title, children, subtitle, img }) => {
+  return (
+    <>
+      <Flex flexWrap="wrap" justifyContent="space-between" mt="10%">
+        <Flex direction="column" w="60%">
+          <Box>
+            <Text fontSize="80px" fontWeight="bold" textDecoration="underline">
+              {title}
+            </Text>
+          </Box>
+          <Box mt="20" w="90%">
+            <Box>
+              <Text fontSize="3xl">{subtitle}</Text>
+            </Box>
+            <Box mt="7" ml="4">
+              <Text fontSize="xl">{children}</Text>
+            </Box>
+          </Box>
+        </Flex>
+        <Image src={`/${img}`} alt="" display="block" w="40%" height="100%" mt="10%" />
+      </Flex>
+    </>
+  );
+};
+
+export default TopCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,32 @@
 import { NextPage } from 'next';
 // import Image from 'next/image';
-import { Box, Image, Flex } from '@chakra-ui/react';
+import { Box, Image, Text } from '@chakra-ui/react';
+import TopCard from 'src/components/top/TopCard';
 
 const Home: NextPage = () => {
   return (
     <>
-      <Box>
-        <Image src="/tokyo.jpg" alt="" minW="100%" height="500px" />
+      <Box position="relative">
+        <Image src="/top.jpg" alt="" Width="100%" maxH="600" />
+        <Text position="absolute" bottom="18%" color="white" fontSize="6xl" left="5%">
+          Is there anyone who wants to
+        </Text>
+        <Text position="absolute" bottom="9%" color="white" fontSize="6xl" left="20%">
+          move with genuine satisfaction?
+        </Text>
+        <Text position="absolute" bottom="5%" color="white" fontSize="2xl" left="15%" as="samp">
+          心から満足できる引っ越しをしたい方いませんか？
+        </Text>
       </Box>
 
-      <Box mr="16" ml="16" mt="8" mb="8">
-        <Flex flexWrap="wrap" justifyContent="space-between" alignItems="center">
-          <Image src="/tokyo.jpg" alt="" display="block" w="48%" height="auto" />
-          <Box w="48%" textAlign="center">
-            <p>みんなの声が聴けるよ</p>
-          </Box>
-        </Flex>
+      <Box mr="40" ml="40" mt="10" mb="20">
+        <TopCard title="Purpose" subtitle="各地域の総合レビュー" img="tokyo.jpg">
+          東京、引っ越しの際に東京がどんな場所なのか東京に住んでる方の素直な意見を見ることができ、地域についてのイメージをつかめます
+        </TopCard>
 
-        <Flex flexWrap="wrap" justifyContent="space-between" alignItems="center">
-          <Box w="48%" textAlign="center">
-            <p>みんなの疑問を解決するよ</p>
-          </Box>
-          <Image src="/tokyo.jpg" alt="" display="block" w="48%" height="auto" />
-        </Flex>
+        <TopCard title="Review" subtitle="様々な観点からの素直な意見" img="review.jpg">
+          治安、物価、交通など様々な項目に分けて住んでる皆さんの意見や感想など確認できます。
+        </TopCard>
       </Box>
     </>
   );
