6a4e061c6bd46f2171c55699113bddcfa4d86b03
yoshida874 <ud8mnzdd@gmail.com>
2022-02-02 06:45:27 +0900

    fix: レビューのboxのデザイン修正

--- src/components/areaDetail/ReviewBox.tsx
+++ src/components/areaDetail/ReviewBox.tsx
@@ -18,27 +18,31 @@ const ReviewBox: VFC<Props> = ({ review, categories }) => {
   const createdDay = dayjs(review.created_at).format('YYYY-MM-DD');
   const category = categories.filter((category) => category.id === review.category_id);
   return (
-    <Box bg="white" borderBottom="medium solid #FAFAFA" w="100%" pl="4">
-      <HStack flexWrap="wrap">
-        <StarRatings
-          rating={review.evaluation}
-          starRatedColor="#ffd500"
-          numberOfStars={5}
-          starDimension="32px"
-          starSpacing="4px"
-        />
-      </HStack>
-      <Flex>
-        <Flex flexWrap="wrap" alignItems="center">
-          <FaTag color="blue" />
-          {category[0].category_name}
+    <Box bg="white" borderBottom="medium solid #FAFAFA" w="100%">
+      <Box ml="4">
+        <HStack flexWrap="wrap">
+          <StarRatings
+            rating={review.evaluation}
+            starRatedColor="#ffd500"
+            numberOfStars={5}
+            starDimension="32px"
+            starSpacing="4px"
+          />
+        </HStack>
+        <Flex ml="3">
+          <Flex flexWrap="wrap" alignItems="center">
+            <FaTag color="blue" />
+            {category[0].category_name}
+          </Flex>
+          <Flex flexWrap="wrap" ml="4" alignItems="center">
+            <BsStopwatch />
+            {createdDay}
+          </Flex>
         </Flex>
-        <Flex flexWrap="wrap" ml="2" alignItems="center">
-          <BsStopwatch />
-          {createdDay}
-        </Flex>
-      </Flex>
-      <Box mt="3">{review.review_content}</Box>
+        <Box mt="2" ml="2" mb="2">
+          {review.review_content}
+        </Box>
+      </Box>
     </Box>
   );
 };
